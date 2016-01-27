76b5d7fd8b9126f6de0941b2e4d7f544f7402a3e
Adam Romines <adam.romines@gmail.com>
2016-01-27 14:14:22 -0700

    Text changes to remove references to 'young men'

--- about.php
+++ about.php
@@ -14,7 +14,7 @@ $description = "Dr. Ron and Camilla Rabjohns started Invictus Woods in 2011 to p
 
 
     <div class="container">
-      
+
     	<div class="row">
         	<div class="col-md-6">
 				<h1><?php echo $title; ?></h1>
@@ -26,28 +26,28 @@ $description = "Dr. Ron and Camilla Rabjohns started Invictus Woods in 2011 to p
 	  			  <a href="images/InvictusWoods_Brochure_2014.pdf">Click</a> to view the Invictus Woods pdf brochure
 	  		  </p>
 	  		  <br>
-				
-				<p>Invictus Woods is a sober living extended care facility for young adult males ages 18 and older seeking an ongoing recovery program from chemical addiction. We require the successful completion of at least 28 days in a primary care substance abuse treatment center prior to admission.</p>
+
+				<p>Invictus Woods is a sober living extended care facility for men seeking an ongoing recovery program from chemical addiction. We require the successful completion of at least 28 days in a primary care substance abuse treatment center prior to admission.</p>
 				<p>Invictus Woods is located in the heart of the Midwest, 20 miles southwest of Peoria, Illinois. The property sits on 35 acres of serene wilderness. Our rustic housing and rural environment embraces the natural beauty that surrounds us, and we enjoy the splendor of each of the four seasons. Intake and Administration are located on campus.</p>
 				<p>Invictus Woods is strategically located 13 miles from the Peoria Regional Airport (PIA). Airlines that fly to this location include American Airlines, Delta Airlines, United Airlines and Allegiant Airlines.  PIA also allows for the use of private aircraft. Invictus Woods provides transportation to and from the property for participants and visiting family members. Car rental is available at airport during normal business hours.</p>
-				
+
        		</div>
-			
+
 			<div class="col-md-6">
 				<div class='embed-container'><iframe src='//maps.google.com/?ll=40.725892%2C-89.627861&spn=0.743083%2C1.425476&ie=UTF8&z=9&t=roadmap&sll=40.725892%2C-89.627861&sspn=0.743083%2C1.425476&q=County%20Highway%2028%20Canton%2C%20IL%2061520%20(County%20Highway%2028)&output=embed' title='County Highway 28' width='500' height='500' frameborder='0' class='map_embed' scrolling='no'></iframe></div>
 			</div>
-		</div>	
-			
+		</div>
+
 		<div class="row">
 			<div class="col-md-11">
 				<h2>Staff</h2>
 				<h3>Dr. Ronald Rabjohns and Camilla Rabjohns</h3>
 				<p>Dr. Ron Rabjohns is a cardiologist and Camilla Rabjohns is a registered nurse. Camilla is a National Certified Recovery Specialist (NCRS), and received the 2015 Humanitarian Award from the Illinois State Medical Society Alliance. Ron and Camilla know how important an extended care program is in developing maturity and stability after the initial recovery from addiction.  Supporting Invictus Woods is their way of giving back. Ron and Camilla are both active in the local Al-Anon community and frequently lead meetings. Camilla also travels to treatment centers and attends major conferences to continue learning.</p>
-				
+
 			</div>
 		</div>
 
-      	
+
 	 </div>
 
 <?php include 'footer.php'; ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<?php 
+<?php
 $title = "Home";
 include 'header.php'; ?>
 <script>var makeActive = 'nope';</script>
@@ -12,15 +12,15 @@ include 'header.php'; ?>
       <div class="row">
         <div class="col-md-6">
           <h2>Purpose</h2>
-          <p>Invictus Woods is the premier sober living facility in the Midwest. Our purpose is to build a strong foundation for young adult men who are in the early stages of the recovery process. Our focus is on structure and individual goals for each participant. At Invictus Woods we view the disease in a realistic way. We know that recovery is not about being powerless. It is about empowerment. Our goal is to locate what motivates a man and integrate it into his consciousness so he can rebuild his life, move forward and become a productive member of society.</p>
+          <p>Invictus Woods is the premier sober living facility in the Midwest. Our purpose is to build a strong foundation for men who are in the early stages of the recovery process. Our focus is on structure and individual goals for each participant. At Invictus Woods we view the disease in a realistic way. We know that recovery is not about being powerless. It is about empowerment. Our goal is to locate what motivates a man and integrate it into his consciousness so he can rebuild his life, move forward and become a productive member of society.</p>
           <p><a class="btn btn-default" href="philosophy.php" role="button">View details &raquo;</a></p>
         </div>
         <div class="col-md-6">
           <h2>Mission</h2>
-          <p>Invictus Woods Extended Care is committed to providing a safe and caring sober living environment for young adult males (18 and older) recovering from the effects of chemical dependency and other associated behaviors caused by drug and alcohol addiction. Our program builds on the initial therapeutic 12-step process to create a stronger foundation of recovery. We believe recovery does not have to be seen as punishment, and our primary focus is to teach our clients how to stay sober and enjoy the healing process. Set on 35 acres of wooded wilderness outside of Peoria, Illinois, Invictus Woods offers a clean, enjoyable and private sober living community to address the needs of each person. Our program is led by compassionate staff members who recognize there is a kind and loving person inside each one of us.</p>
+          <p>Invictus Woods Extended Care is committed to providing a safe and caring sober living environment for men (18 and older) recovering from the effects of chemical dependency and other associated behaviors caused by drug and alcohol addiction. Our program builds on the initial therapeutic 12-step process to create a stronger foundation of recovery. We believe recovery does not have to be seen as punishment, and our primary focus is to teach our clients how to stay sober and enjoy the healing process. Set on 35 acres of wooded wilderness outside of Peoria, Illinois, Invictus Woods offers a clean, enjoyable and private sober living community to address the needs of each person. Our program is led by compassionate staff members who recognize there is a kind and loving person inside each one of us.</p>
           <p><a class="btn btn-default" href="philosophy.php" role="button">View details &raquo;</a></p>
        </div>
-        
+
       </div>
       <!-- Example row of columns -->
       <div class="row">
@@ -51,7 +51,7 @@ include 'header.php'; ?>
 		  <a href="images/InvictusWoods_Brochure_2014.pdf">
 			  <img src="images/pdf_brochure.png">
 		  </a><br><br>
-		  
+
           <p><a class="btn btn-default" href="images/InvictusWoods_Brochure_2014.pdf" role="button">View brochure &raquo;</a></p>
         </div>
         <div class="col-md-6">
@@ -63,7 +63,7 @@ include 'header.php'; ?>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
       </div>
-	  
+
 
 <?php include 'footer.php'; ?>
 
