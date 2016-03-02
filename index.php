020a4dc410a137d4bbfc09763ed3e03c23de6e18
Adam Romines <adam.romines@gmail.com>
2016-03-02 15:24:46 -0700

    Updates brochure to 2015 version

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<?php 
+<?php
 $title = "Home";
 include 'header.php'; ?>
 <script>var makeActive = 'nope';</script>
@@ -20,7 +20,7 @@ include 'header.php'; ?>
           <p>Invictus Woods Extended Care is committed to providing a safe and caring sober living environment for young adult males (18 and older) recovering from the effects of chemical dependency and other associated behaviors caused by drug and alcohol addiction. Our program builds on the initial therapeutic 12-step process to create a stronger foundation of recovery. We believe recovery does not have to be seen as punishment, and our primary focus is to teach our clients how to stay sober and enjoy the healing process. Set on 35 acres of wooded wilderness outside of Peoria, Illinois, Invictus Woods offers a clean, enjoyable and private sober living community to address the needs of each person. Our program is led by compassionate staff members who recognize there is a kind and loving person inside each one of us.</p>
           <p><a class="btn btn-default" href="philosophy.php" role="button">View details &raquo;</a></p>
        </div>
-        
+
       </div>
       <!-- Example row of columns -->
       <div class="row">
@@ -46,13 +46,13 @@ include 'header.php'; ?>
         <div class="col-md-6">
           <h2>Download Brochure</h2>
           <p>Invictus Woods pdf brochure
-			  <a href="images/InvictusWoods_Brochure_2014.pdf">click</a> to view our pdf brochure
+			  <a href="images/InvictusWoods_Brochure_2015.pdf">click</a> to view our pdf brochure
 		  </p>
-		  <a href="images/InvictusWoods_Brochure_2014.pdf">
+		  <a href="images/InvictusWoods_Brochure_2015.pdf">
 			  <img src="images/pdf_brochure.png">
 		  </a><br><br>
-		  
-          <p><a class="btn btn-default" href="images/InvictusWoods_Brochure_2014.pdf" role="button">View brochure &raquo;</a></p>
+
+          <p><a class="btn btn-default" href="images/InvictusWoods_Brochure_2015.pdf" role="button">View brochure &raquo;</a></p>
         </div>
         <div class="col-md-6">
           <h2>Costs</h2>
@@ -63,7 +63,7 @@ include 'header.php'; ?>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
       </div>
-	  
+
 
 <?php include 'footer.php'; ?>
 
