57b66c171e0e4a8050f914c8023c34856b8ef7e3
Adam Romines <adam.romines@gmail.com>
2017-02-01 19:32:24 -0700

    Fourth annual Golf Outing

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include 'header.php'; ?>
 			<img src='images/ball_tee.png' class="left">
 			<div id="insideGolf">
 
-				<h3>Save the date! <span class="spoof">I.W.E.C Third Annual <b>Golf for Recovery</b></span> Saturday, June 11th - Country Club of Peoria</h3>
+				<h3>Save the date! <a href="images/IW-golf-for-recovery.pdf">I.W.E.C Fourth Annual <b>Golf for Recovery</b></a> Saturday, June 10th - Country Club of Peoria</h3>
 
 			</div>
 			<img src='images/golfcart.png' class="right">
